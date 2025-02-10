6da25b9a11433801e31247307d2500f340e400fe
GabyAriza21 <gabriela.astorquiza@campusucc.edu.co>
2025-02-10 13:36:55 +0000

    Variables

---
+++ Semana_1/Variables.java
@@ -0,0 +1,25 @@
+public class Variables {
+    public static void main(String[] args) {
+        //decalracion de una variable de tipo entero
+        int edad = 25;
+        //declaracion de una variable de tipo real
+        double altura = 1.62;
+        //declaracion de una variable de tipo cadena
+        String nombre = "Gaby";
+        //declracion de una variable tipo boolean
+        boolean esEstudiante = true;
+        /* 
+        con la instruccion Soutln se mostrara
+        las variables y cada una de ellas estara
+        concatenadas con el signo (+) y la leyenda
+        */
+        System.out.println("Nombre: " + nombre + 
+        ", Edad: " + edad + ", Altura: " + altura + 
+        ", Estudiante: " + esEstudiante);
+       
+
+
+        
+    }
+    
+}

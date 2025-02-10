0548320665a0a303971eca5e5f293fc0f2249fce
GabyAriza21 <gabriela.astorquiza@campusucc.edu.co>
2025-02-10 14:58:37 +0000

    corregi error

--- Semana_1/Bucles.java
+++ Semana_1/Bucles.java
@@ -2,14 +2,14 @@ public class Bucles {
     public static void main(String[] args) {
         //for
         for(int i = 1; i <= 5; i++){
-            System.out.println(i = ": Programación orientada a objetos");
-        } 
-
+            System.out.println(i + ": POO");
+        }
+    
         System.out.println("------------------------------------------");
         //while
         int j = 1;
         while (j <= 5){
-            System.out.println(j + ":POO");
+            System.out.println(j + ": POO");
             j++;
         }
     }

1ab61f34ab1295f2bd895e5a82496d508cf6db53
GabyAriza21 <gabriela.astorquiza@campusucc.edu.co>
2025-02-10 13:45:02 +0000

    Condicionales

--- Semana_1/Condicionales.java
+++ Semana_1/Condicionales.java
@@ -1,3 +1,11 @@
 public class Condicionales {
+    public static void main(String[] args) {
+        int numero = -5;
+        if(numero > 0){
+            System.out.println("El numero es positivo");
+        } else{
+            System.out.println("El numero es negativo");
+        }
+    }
     
 }

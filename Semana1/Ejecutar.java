aad84e2fdcde88198e151c0718560063ff436e35
GabyAriza21 <gabriela.astorquiza@campusucc.edu.co>
2025-03-17 14:09:56 +0000

    correccion errores

--- Semana1/Ejecutar.java
+++ Semana1/Ejecutar.java
@@ -1,8 +1,7 @@
 public class Ejecutar {
-    public static void main(String[] args){
-        
-        //Mostrar en pantalla
-        System.out.println("Hola Mundo."); 
-    
-    } 
+    public static void main(String[] args) {
+        // creacion del objeto (instanciar (operador new) el objeto)
+        Estudiante objEst1 = new Estudiante(1, "Gabriela", "Astorquiza", 18, "Ingenieria");
+        Estudiante objEst2 = new Estudiante(2, "Juan", "Perez", 20, "Ingenieria");
+    }
 }
---
+++ Semana1/EjecutarClase1.java
@@ -0,0 +1,8 @@
+public class EjecutarClase1 {
+    public static void main(String[] args){
+        
+        //Mostrar en pantalla
+        System.out.println("Hola Mundo."); 
+    
+    } 
+}

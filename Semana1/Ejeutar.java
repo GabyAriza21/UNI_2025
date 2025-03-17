c9f4e676a6929898cdcf4951dc0b41861ce4657c
GabyAriza21 <gabriela.astorquiza@campusucc.edu.co>
2025-03-17 14:03:02 +0000

    creacion de los objetos

---
+++ Semana1/Ejeutar.java
@@ -0,0 +1,11 @@
+public class Ejeutar {
+    public static void main(String[] args) {
+        // creacion del objeto (instanciar (operador new) el objeto)
+        Estudiante objEst1 = new Estudiante(1,"Gabriela", "Astorquiza", 18, "Ingenieria");
+        Estudiante objEst2 = new Estudiante(2,"Juan", "Perez", 20, "Ingenieria");        
+        
+ 
+
+
+    }
+}

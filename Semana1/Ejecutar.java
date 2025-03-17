15ad7c44eac70c8d0318700e700e527c07e04417
GabyAriza21 <gabriela.astorquiza@campusucc.edu.co>
2025-03-17 14:22:01 +0000

    Correccion edad

--- Semana1/Ejecutar.java
+++ Semana1/Ejecutar.java
@@ -1,7 +1,17 @@
 public class Ejecutar {
     public static void main(String[] args) {
         // creacion del objeto (instanciar (operador new) el objeto)
+        // llave que accede a la clase Estudiante
+        // se da vida con la clase Estudiante
+        // accede a los metodos de la clase Estudiante
         Estudiante objEst1 = new Estudiante(1, "Gabriela", "Astorquiza", 18, "Ingenieria");
         Estudiante objEst2 = new Estudiante(2, "Juan", "Perez", 20, "Ingenieria");
+
+        System.out.println(objEst1); // toSting
+        System.out.println(objEst2);
+
+        // modificar la edad del estudiante
+        objEst1.setEdad(19);
+        System.out.println(objEst1);
     }
 }

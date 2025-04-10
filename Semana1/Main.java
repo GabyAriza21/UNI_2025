ce9a73fb20541b432f527ebf04e2bc0fb641f180
GabyAriza21 <gabriela.astorquiza@campusucc.edu.co>
2025-04-10 15:46:05 +0000

    prueba 2

--- Semana1/Main.java
+++ Semana1/Main.java
@@ -11,5 +11,5 @@ public class Main {
         PrecioTotal solucion = new PrecioTotal(equipaje);
         solucion.mostrarTotales();
     }
-    
+
 }
---
+++ Semana1/Main2.java
@@ -0,0 +1,13 @@
+public class Main2 {
+    public static void main(String[] args) {
+        Equipaje equipaje[] = new Equipaje[4];
+
+        equipaje[0] = new Bodega(300.0, 5.0); // pesadito
+        equipaje[1] = new Bodega(50.0, 1.0); // pequeñito
+        equipaje[2] = new Cabina(); // valores defecto
+        equipaje[3] = new Cabina(500); // precio base solamente
+
+        PrecioTotal prueba = new PrecioTotal(equipaje);
+        prueba.mostrarTotales();
+    }
+}

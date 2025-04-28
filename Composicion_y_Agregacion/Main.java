5341dc64e3600e70fbc5a31e10dbbf6265d1281e
GabyAriza21 <gabriela.astorquiza@campusucc.edu.co>
2025-04-28 13:48:29 +0000

    ejecutar herencia y main agregacion y composicion

---
+++ Composicion_y_Agregacion/Main.java
@@ -0,0 +1,17 @@
+package Composicion_y_Agregacion;
+
+public class Main {
+    public static void main(String[] args) {
+        Cuenta cuenta = new Cuenta(1234567892, 1000.0); 
+
+        Cliente cliente = new Cliente (1031650195, "Gaby");
+
+        System.out.println(cuenta);
+        System.out.println(cliente);
+
+
+
+
+        
+    }
+}

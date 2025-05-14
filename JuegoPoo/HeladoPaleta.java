f3cbbbc8f1c11fdf8656a3f86099bf6b04ac7a28
GabyAriza21 <gabriela.astorquiza@campusucc.edu.co>
2025-05-14 20:43:02 +0000

    correccion atributo sin sentido de helado paleta

--- JuegoPoo/HeladoPaleta.java
+++ JuegoPoo/HeladoPaleta.java
@@ -1,15 +1,13 @@
 package JuegoPoo;
 
 public class HeladoPaleta extends Personaje {
-    private String tipoCono;
 
     public HeladoPaleta(String nombre, int puntosVida, String tipoCono, Topping topping) {
         super(nombre, puntosVida, 30, 10, topping);
-        this.tipoCono = tipoCono;
     }
 
     public void atacar(Personaje enemigo){
-        System.out.println("El helado " + getNombre() + " ataca con su cono de tipo " + tipoCono + "!");
+        System.out.println("El helado " + getNombre() + " ataca!");
         super.atacar(enemigo);
     }
         

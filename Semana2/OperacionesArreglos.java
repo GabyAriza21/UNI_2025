edb64de492c8f01b5e09604b1430476e3fba53ef
GabyAriza21 <gabriela.astorquiza@campusucc.edu.co>
2025-08-04 14:12:56 +0000

    metodo del arreglo

---
+++ Semana2/OperacionesArreglos.java
@@ -0,0 +1,16 @@
+public class  Calse1OperacionesArreglos {
+
+    //metodo para llenara rreglo de enteros
+    public int[] llenarArregloEnteros(int tam){
+
+        Random rand = new Random();
+
+        int[] numeros = new int[tam];
+        for (int i = 0; i< numeros.length; i++){
+            numero[i] = rand.nextInt(100 -1) + 1;
+
+        }
+            return 0; 
+        
+    }
+}

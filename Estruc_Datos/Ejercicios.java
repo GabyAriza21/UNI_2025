307646e07cced636dcb1923595f28c0a7a7528b9
GabyAriza21 <gabriela.astorquiza@campusucc.edu.co>
2025-08-07 17:30:29 +0000

    ejercicios

--- Estruc_Datos/Ejercicios.java
+++ Estruc_Datos/Ejercicios.java
@@ -4,7 +4,7 @@ public class Ejercicios {
     public static void main(String[] args) {
         //Sumar los elementos de un arreglo de enteros   
         int[] arreglo = {7, 10, 11, 15, 8};
-        int suma = 0;
+        int suma = 0; // variable entera simple: almacena el resultado de la suma
 
         for(int i = 0; i < arreglo.length; i++) {
            suma += arreglo[i];
@@ -12,6 +12,3 @@ public class Ejercicios {
         System.out.println("Suma: " + suma);
     }
 }
-
-
-    

cedb11996cb9d3a837bcc0f43445f78ce754b8a2
GabyAriza21 <gabriela.astorquiza@campusucc.edu.co>
2025-08-07 19:21:10 +0000

    inicio arreglo 3

--- Estruc_Datos/Ejercicios.java
+++ Estruc_Datos/Ejercicios.java
@@ -2,32 +2,31 @@ package Estruc_Datos;
 
 public class Ejercicios {
     public static void main(String[] args) {
-        //Sumar los elementos de un arreglo de enteros   
-        int[] arreglo = {7, 10, 11, 15, 8};
+        // Sumar los elementos de un arreglo de enteros
+        int[] arreglo = { 7, 10, 11, 15, 8 };
         int suma = 0; // variable entera simple: almacena el resultado de la suma
 
-        for(int i = 0; i < arreglo.length; i++) {
-           suma += arreglo[i];
+        for (int i = 0; i < arreglo.length; i++) {
+            suma += arreglo[i];
         }
         System.out.println("Suma: " + suma);
 
         System.out.println("-----------------------------------------");
 
-        //Obtener el promedio de un arreglo de enteros
-        int[] arreglo2 = {25, 26, 78, 41, 10};
-        int suma2 = 0; 
+        // Obtener el promedio de un arreglo de enteros
+        int[] arreglo2 = { 25, 26, 78, 41, 10 };
+        int suma2 = 0;
 
-        for(int i = 0; i < arreglo2.length; i++) {
-          suma2 += arreglo2[i];  
+        for (int i = 0; i < arreglo2.length; i++) {
+            suma2 += arreglo2[i];
         }
-        double promedio = suma2/arreglo2.length;
-        System.out.println("promedio: " +  promedio);
+        double promedio = suma2 / arreglo2.length;
+        System.out.println("promedio: " + promedio);
 
-        
+        System.out.println("-------------------------------------------");
 
+        // Obtener los numeros repetidos de un arreglo de enteros
+        int[] arreglo3 = {12, 34, 32, 12, 54, 32};
 
-
-
-    }
-}
-
+            }
+        }

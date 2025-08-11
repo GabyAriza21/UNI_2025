19849b16e296033ae46a4067bc45aec965ad08a7
GabyAriza21 <gabriela.astorquiza@campusucc.edu.co>
2025-08-11 15:03:42 +0000

    trabajo en casa

--- Estruc_Datos/EjecutarEstudiante.java
+++ Estruc_Datos/EjecutarEstudiante.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class EjecutarEstudiante {
     public static void main(String[] args) {
 
-        //creacion de los objetos
+        // creacion de los objetos
         Estudiante objEst1 = new Estudiante(909876, "juanito", "perez", "ingenieiria");
         Estudiante objEst2 = new Estudiante(909877, "juanita", "perea", "ingenieiria");
         Estudiante objEst3 = new Estudiante(909878, "sarita", "prado", "ingenieiria");
@@ -14,20 +14,34 @@ public class EjecutarEstudiante {
         System.out.println(objEst2);
         System.out.println(objEst3);
 
-        System.out.println("---------------------------------------------------------------------------------------------------------------");
+        System.out.println(
+                "---------------------------------------------------------------------------------------------------------------");
 
-        //crear arreglo de objetos
+        // crear arreglo de objetos
         // int[] a = new int[5]
-        Estudiante est[] = new Estudiante[3]; //arreglo de objetos 
+        Estudiante est[] = new Estudiante[5]; // arreglo de objetos
 
-        est[0] = objEst1; 
+        est[0] = objEst1;
         est[1] = objEst2;
         est[2] = objEst3;
 
+        // forma2
+
+        est[3] = new Estudiante(789654, "juli", "torres", "derecho");
+        est[4] = new Estudiante(789655, "toño", "zuluaga", "Contaduria");
+
         System.out.println(Arrays.toString(est));
 
-        System.out.println("--------------------------------- FORMA DOS --------------------------------------------- ");
+        System.out.println("--------------------------------------------");
+
+        for (Estudiante estudiante : est) {
+            System.out.println(estudiante);
+        }
+
+        System.out.println("--------------------------------------------");
+        for (int i = 0; i < est.length; i++) {
+            System.out.println(est[i]);
 
         }
     }
-    
+}
--- Estruc_Datos/Estudiante.java
+++ Estruc_Datos/Estudiante.java
@@ -1,33 +1,38 @@
 package Estruc_Datos;
 
 public class Estudiante {
-    //atrubutos clase
-    private int id; 
+    // atrubutos clase
+    private int id;
     private String nombre;
-    private String apellido; 
+    private String apellido;
     private String facultad;
-    
-    //constructor clase = MISMO NOMBRE DE LA CLASE
-    public Estudiante(int id, String nombre, String apellido, String facultad){
-        this.id = id; 
-        this.nombre = nombre; 
-        this.apellido = apellido; 
-        this.facultad = facultad; 
+
+    // constructor clase = MISMO NOMBRE DE LA CLASE
+    public Estudiante(int id, String nombre, String apellido, String facultad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.facultad = facultad;
     }
 
-    public int getId(){
-        return id; 
+    public int getId() {
+        return id;
     }
 
-    public void matricularCursos(int id, String[] cursos){
+    public void matricularCursos(int id, String[] cursos) {
         String cad = "";
-        for(int i = 0; i < cursos.length; i++){
+        for (int i = 0; i < cursos.length; i++) {
             cad += cursos[i] + " ";
         }
         System.out.println("id: " + id + "Cursos Matriculados: " + cad);
     }
 
-    public String toString(){
-        return "Estudiante { id:" + id +  " Nombre: " + nombre + "Apellido: " + apellido + " Facultad: " + facultad + "}"; //maquetar(estetica)
+    public String toString() {
+        return "Estudiante { id:" + id + " Nombre: " + nombre + "Apellido: " + apellido + " Facultad: " + facultad
+                + "}"; // maquetar(estetica)
     }
+
+    //implementar el metodo +matricularCursos(id:int, cursos[]:String):void
+    //implementar un metodo que permita saber cuantos estudiantes hay en cada facultad
+    //implementar un metodo que permita saber cuantos estudiantes hay en cad curso 
 }
